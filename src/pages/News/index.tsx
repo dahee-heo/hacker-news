b03a34b0cb480d95ce9aca14adfc5ed40f21203e
dahee-heo <hdahee.9@gmail.com>
2026-04-29 23:22:05 +0900

    feat: news 페이지 마크업

--- src/pages/News/index.tsx
+++ src/pages/News/index.tsx
@@ -1,5 +1,49 @@
+
 const News = () => {
-  return <div>News</div>;
+  return (
+    <div>
+      <h1 className="text-2xl font-bold mb-4">AIPIA News</h1>
+      <div>
+        <ul>
+          <li>Top</li>
+          <li>New</li>
+          <li>Best</li>
+        </ul>
+        <ul>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <div>title</div>
+              <div>author</div>
+              <div>0000-00-00</div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <div>title</div>
+              <div>author</div>
+              <div>0000-00-00</div>
+            </div>
+          </li>
+          <li>
+            <div>
+              <img src="" alt="" />
+            </div>
+            <div>
+              <div>title</div>
+              <div>author</div>
+              <div>0000-00-00</div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default News;
