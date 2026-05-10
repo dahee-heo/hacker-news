5eb9b018e46346eccb69a5e37afd6e9bc3114e1e
dahee-heo <hdahee.9@gmail.com>
2026-05-10 21:15:39 +0900

    feat: 상세페이지 스켈레톤 추가

--- src/pages/News/detail.tsx
+++ src/pages/News/detail.tsx
@@ -10,7 +10,17 @@ const NewsDetail = () => {
     queryFn: () => reqGetStory(Number(id!)),
   });
 
-  if (isPending) return <div>Loading...</div>;
+  if (isPending)
+    return (
+      <div className="p-4 animate-pulse">
+        <div className="h-6 bg-gray-200 rounded w-3/4 mb-3" />
+        <div className="h-6 bg-gray-200 rounded w-1/2 mb-2" />
+        <div className="h-4 bg-gray-200 rounded w-1/4" />
+        <hr className="border-gray-200 my-4" />
+        <div className="h-4 bg-gray-200 rounded w-1/3 mb-4" />
+        <div className="h-6 bg-gray-200 rounded w-16" />
+      </div>
+    );
 
   return (
     <div className="p-4">
