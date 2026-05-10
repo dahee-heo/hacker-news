c9bfab9c53203b2fec2eaae90d71e4d606d7f101
dahee-heo <hdahee.9@gmail.com>
2026-05-10 16:56:11 +0900

    feat: list hover 효과 추가

--- src/components/News/NewsItem.tsx
+++ src/components/News/NewsItem.tsx
@@ -7,12 +7,16 @@ interface NewsItemProps {
 
 const NewsItem = ({ title, author, date, imageUrl }: NewsItemProps) => {
   return (
-    <li className="flex gap-[12px] py-4">
+    <li className="flex gap-[12px] py-4 cursor-pointer group">
       <div className="w-[64px] h-[64px] bg-gray-200 flex-shrink-0 overflow-hidden rounded">
-        <img src={imageUrl} alt={title} />
+        <img
+          className="transition-transform duration-300 group-hover:scale-110"
+          src={imageUrl}
+          alt={title}
+        />
       </div>
       <div>
-        <div className="text-xl font-bold">{title}</div>
+        <div className="text-xl font-bold duration-300 underline-offset-2 group-hover:underline">{title}</div>
         <div className="text-sm text-gray-600">{author}</div>
         <div className="text-sm text-gray-600">{date}</div>
       </div>
